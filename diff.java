69a94c0ab08810f5a05a5bd75501fc731c698a30
akash <akashfnp@gmail.com>
2022-06-18 10:44:41 +0530

    data added in diff.java

--- diff.java
+++ diff.java
@@ -1,3 +1,8 @@
 public class diff {
-    
+    public static void main(String[] args) {
+        int a = 102;
+        int b = 10;
+        int diff = a-b;
+        System.out.println("Diffrence of a - b = "+ diff);
+    }
 }
