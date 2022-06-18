3840b8c79859182f1f926ecb03a4777c75ef3f8e
akash <akashfnp@gmail.com>
2022-06-18 09:57:57 +0530

    Multiply.java added in akash/Multiply branch

---
+++ Multiply.java
@@ -0,0 +1,8 @@
+public class Multiply {
+    public static void main(String[] args) {
+        int a = 45;
+        int b = 34;
+        int mul = a+b;
+        System.out.println("Multiply of a and b = "+mul);
+    }
+}
