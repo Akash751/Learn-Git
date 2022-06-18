2831f50187b6601f494eb23b9631e8a88bf9bb2c
akash <akashfnp@gmail.com>
2022-06-18 09:30:43 +0530

    One Class added and some modification done in sum.java

--- sum.java
+++ sum.java
@@ -2,6 +2,7 @@ public class sum {
     public static void main(String[] args) {
         int a = 45;
         int b = 34;
-        System.out.println("Sum of a and b = " + a+b);
+        int sum = a+b;
+        System.out.println("Sum of "+ a + " and " + b + " = " + sum);
     }
 }
